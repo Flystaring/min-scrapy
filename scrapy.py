8cc21f6b275586558c4c4b1db6555f4959597aac
Flystaring <xgmgkiss@gmail.com>
2018-10-24 03:48:50 +0800

    完善代码

--- scrapy.py
+++ scrapy.py
@@ -1,9 +1,32 @@
 # -*- coding:utf-8 -*-
 from bs4 import BeautifulSoup
 import requests
+import datetime
  
+def selectComments(commentTitle, keywords, delFlag=True):
+    for item in keywords:
+        if item in commentTitle:
+            if delFlag: 
+                flag = True
+            else:
+                flag = False
+            break
+        else:
+            if delFlag: 
+                flag = False
+            else:
+                flag = True
+    return flag
+
+
 if __name__ == '__main__':
-    target = 'http://guba.eastmoney.com/list,waihui_1.html'
+
+    #评论的起止时间段
+    startTime = datetime.datetime(2015, 10, 1)
+    endTime = datetime.datetime(2018, 9, 30)
+
+    pageNum = 1
+    target = 'http://guba.eastmoney.com/list,waihui,f_'+ pageNum +'.html'
     req = requests.get(url=target)
     html = req.text
     #print(html)
@@ -12,31 +35,61 @@ if __name__ == '__main__':
     titles = bf.find_all("div", class_="articleh")
 
     #解析每个节点, 切分每个帖子的标题和URL以及作者
-    for item in titles:        
-        spanTitle = item.find("span", class_="l3")
+    for item in titles:
+        spanTitle = item.find("span", class_="l3") 
         if spanTitle:
-            #当讨论,新闻,悬赏帖子时,跳过爬取
-            em = spanTitle.find("em")
-            if em and "settop" in em["class"]:
-                continue
-            if em and "hinfo" in em["class"]:
-                continue
+            aTag = spanTitle.find("a")
+            commentTitle = aTag['title'] 
 
+            #当讨论,新闻,悬赏帖子,或者title含有图片时,跳过该帖子
+            em = spanTitle.find("em") 
+            if em:
+                continue
+                    
+            #剔除标题中含有指定关键字的帖子
+            delKeyWords = ['贬基','攻略','维权']         
+            skipFlag = selectComments(commentTitle,delKeyWords,True)
+            if skipFlag:
+                continue
+            
+            
+            #保留带有外汇,欧,美,中国,日,镑,情绪,人民币,汇率的帖子
+            cotKeyWords = ['外汇','欧','美','中国','日','镑','情绪','人民币','汇率']
+            skipFlag = selectComments(commentTitle,cotKeyWords,False)
+            if skipFlag:
+                continue
+        
+            #print(commentTitle)   
             #获取每个帖子的跳转链接
             title = spanTitle.find("a")
             aUrl = 'http://guba.eastmoney.com' + title["href"]
             
             #获取每个帖子的作者
             spanAuthor = spanTitle.next_sibling
-            aAuthor = spanAuthor.find("a").string
+            print(spanAuthor)
+            try:
+                aAuthor = spanAuthor.find("a").string
+            except Exception:
+                print("作者未知")
             a = {"url":aUrl,"Author":aAuthor}
             #print(a["url"])
             req = requests.get(url=a["url"])
             html = req.text
             #print(html)
             bf = BeautifulSoup(html,"lxml")
-            contents = bf.find("div", class_="stockcodec")
-           
-            print(contents.get_text())
+
+            #评论在选择的时间段内, 则获取该帖子内容 
+            commentTimeTag = bf.find("div", class_="zwfbtime")
+            commentTime = commentTimeTag.get_text().split(" ")[1].split("-")
+            commentTime = datetime.datetime(int(commentTime[0]), int(commentTime[1]), int(commentTime[2]))
+            if ((commentTime - startTime).days >= 0 and (commentTime - endTime).days <= 0):
+                print(commentTime)
+                contents = bf.find("div", class_="stockcodec")
+                #print(contents.get_text())
+            #当时间小于开始时间时, 后面的帖子不获取
+            elif((commentTime - startTime).days < 0):
+                break
+            
+            
 
             
